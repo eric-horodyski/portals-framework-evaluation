abce07c02ca6c45d347bc81fd2df093c4124e47a
Eric Horodyski <horodyski@ionic.io>
2023-01-04 09:51:11 -0500

    add all frameworks to an Android view

--- android/java/app/src/main/java/io/ionic/cs/portals/portalsplayground/PlaygroundApp.java
+++ android/java/app/src/main/java/io/ionic/cs/portals/portalsplayground/PlaygroundApp.java
@@ -2,6 +2,10 @@ package io.ionic.cs.portals.portalsplayground;
 
 import android.app.Application;
 
+import androidx.annotation.IdRes;
+
+import io.ionic.portals.Portal;
+import io.ionic.portals.PortalFragment;
 import io.ionic.portals.PortalManager;
 
 public class PlaygroundApp extends Application {
@@ -11,8 +15,8 @@ public class PlaygroundApp extends Application {
         super.onCreate();
         PortalManager.register(getString(R.string.portals_api_key));
 
-        String name = "playground";
-        String startDir = "portals/playground";
-        PortalManager.newPortal(name).setStartDir(startDir).create();
+        PortalManager.newPortal("portal1").setStartDir("portals/angular").create();
+        PortalManager.newPortal("portal2").setStartDir("portals/react").create();
+        PortalManager.newPortal("portal3").setStartDir("portals/vue").create();
     }
 }
--- android/java/app/src/main/java/io/ionic/cs/portals/portalsplayground/ui/PortalsActivity.java
+++ android/java/app/src/main/java/io/ionic/cs/portals/portalsplayground/ui/PortalsActivity.java
@@ -1,10 +1,14 @@
 package io.ionic.cs.portals.portalsplayground.ui;
 
+import androidx.annotation.IdRes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
 import io.ionic.cs.portals.portalsplayground.R;
+import io.ionic.portals.Portal;
+import io.ionic.portals.PortalFragment;
+import io.ionic.portals.PortalManager;
 
 public class PortalsActivity extends AppCompatActivity {
 
@@ -13,5 +17,16 @@ public class PortalsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_portals);
         setTitle(R.string.title_portals);
+
+        renderPortal("portal1", R.id.portal1);
+        renderPortal("portal2", R.id.portal2);
+        renderPortal("portal3", R.id.portal3);
+    }
+
+    public void renderPortal(String name, @IdRes int containerViewId) {
+        Portal portal = PortalManager.getPortal(name);
+        PortalFragment fragment = new PortalFragment(portal);
+
+        getSupportFragmentManager().beginTransaction().replace(containerViewId, fragment).commit();
     }
 }
